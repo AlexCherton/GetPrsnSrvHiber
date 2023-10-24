1c2e322adf41fc4db1a82353b2420144aae60f2f
Alex <Alex_General2002@mail.ru>
2023-10-25 02:35:27 +0400

    db add

---
+++ db/cr_tbl_person_add_vls.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE public.person
+(
+    id         serial,
+    first_name character varying(50) NOT NULL,
+    last_name  character varying(50) NOT NULL,
+    age        integer               NOT NULL,
+    CONSTRAINT person_id_pk PRIMARY KEY (id)
+);
+
+insert into person (first_name, last_name, age)
+values ('Иван', 'Петров', 23);
+
+select * from person
